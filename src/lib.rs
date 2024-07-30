4059d73bbbc1494197d4f9550f78b6c04f61f3ea
kenken <kengoviolinphoto@gmail.com>
2024-07-30 21:50:36 +0900

    Add defining soncat command

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
+use clap::{parser::ValuesRef, Arg, ArgAction, ArgMatches, Command};
 use std::error::Error;
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
-pub fn run() -> MyResult<()> {
-    println!("Hello, world!");
+pub fn run(config: Config) -> MyResult<()> {
+    dbg!(&config);
     Ok(())
 }
 
@@ -12,4 +13,43 @@ pub struct Config {
     files: Vec<String>,
     number_lines: bool,
     number_nonblank_lines: bool,
+}
+
+pub fn get_args() -> MyResult<Config> {
+    let cli: ArgMatches = Command::new("soncat")
+        .author("sontixyou")
+        .version("0.0.1")
+        .about("print file's text")
+        .arg(
+            Arg::new("files")
+                .value_name("FILE_NAMES")
+                .help("Input files name")
+                .num_args(1..)
+                .required(true),
+        )
+        .arg(
+            Arg::new("number_lines")
+                .short('n')
+                .long("number")
+                .help("Number all output lines")
+                .action(ArgAction::SetTrue),
+        )
+        .arg(
+            Arg::new("number_nonblank_lines")
+                .short('b')
+                .long("number-nonblank")
+                .help("Number only nonempty output lines")
+                .action(ArgAction::SetTrue),
+        )
+        .get_matches();
+
+    Ok(Config {
+        files: cli
+            .get_many::<String>("files")
+            .unwrap()
+            .map(|s| s.to_string())
+            .collect(),
+        number_lines: cli.get_flag("number_lines"),
+        number_nonblank_lines: cli.get_flag("number_nonblank_lines"),
+    })
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    if let Err(e) = soncat::run() {
+    if let Err(e) = soncat::get_args().and_then(soncat::run) {
         eprintln!("Error: {}", e);
         std::process::exit(1);
     }
